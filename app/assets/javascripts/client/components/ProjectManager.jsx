d75ceef403b1474c6553eee03b1630fb5e7bacc0
fmsuvM <k222.tkd.fms@gmail.com>
2017-03-08 16:13:26 +0900

    a little change

--- app/assets/javascripts/client/components/ProjectManager.jsx
+++ app/assets/javascripts/client/components/ProjectManager.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component, PropTypes} from 'react';
 import Debug from 'debug';
 
 import Navigation from './Navigation';
@@ -7,18 +7,21 @@ import Footer from './Footer';
 
 const debug = Debug('fabnavi:jsx:ProjectManager');
 
-export default class ProjectManager extends React.Component {
+export default class ProjectManager extends Component {
   constructor(props) {
+    console.log('--- project manager is generated');
+    console.log('props is below');
+    console.dir(props);
     super(props);
   }
 
   render() {
     return (
       <div className="body">
-        <Navigation />// 上
-        <SearchBar />// 右斜上
-          {this.props.children}// これ何
-        <Footer />// 一番下
+        <Navigation />
+        <SearchBar />
+          {this.props.children}
+        <Footer />
       </div>
     );
   }
