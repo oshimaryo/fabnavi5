4ef69305a2bf0dcd5e6653ef34b95344a20494f0
fmsuvM <k222.tkd.fms@gmail.com>
2017-03-09 10:38:35 +0900

    change components in render function

--- app/assets/javascripts/client/components/ProjectList.jsx
+++ app/assets/javascripts/client/components/ProjectList.jsx
@@ -1,11 +1,12 @@
 // defaultで表示するcomponent
-import React, {Component, PropTypes} from 'react';
+import React, {Component, PropTypes, cloneElement} from 'react';
 import { connect } from 'react-redux';
 import ReactPaginate from 'react-paginate'
 import Debug from 'debug';
 
 import ProjectElement from '../components/ProjectElement';
 import Pagination from '../components/Pagination.jsx';
+import ShowingResults from '../components/ShowingResults.jsx';
 
 const debug = Debug('fabnavi:jsx:ProjectList');
 // component
@@ -18,41 +19,85 @@ class ProjectList extends Component {
 
   render() {
     const selector = this.props.selector;
-    console.log('--- projectList is generated ---' );
-    console.dir(this.props);
+    // console.log('--- projectList is generated ---' );
+    // console.dir(this.props);
     console.log('props.selector');
-    console.log(this.props.selector);
+    console.dir(this.props);
+    /* 
+    return (
+      <div>
+        <Pagination data={this.props.projects}>
+          <ShowingResults />
+        </Pagination>
+      </div>
+    )
+    */
+    /*
+    <div>
+        <div className="projects">
+          {this.props.projects.map((project, index) => {
+            console.log('project contents');
+            console.dir(project);
+            console.log('index contents');
+            console.dir(index);
+            return (
+              <ProjectElement
+                  key={index}
+                  project={project}
+                  isSelected={selector.index == index}
+                  isOpenMenu={selector.index == index && selector.openMenu}
+                  menuIndex={selector.menuIndex}
+                  menuType={selector.menuType} /> 
+            )
+          }
+          )}
+        </div>
+        <Pagination />
+      </div>
+    */
+    /* 
+    <div className="projects">
+          {this.props.projects.map((project, index) => {
+            // console.log('project contents');
+            // console.dir(project);
+            // console.log('index contents');
+            // console.dir(index);
+            // この中のタグが描画される
+            return (
+              <div>
+                <UpdatePagination key={index} data={this.props.projects}>
+                  <ShowingResults />
+                </UpdatePagination>
+              </div>
+            )
+          }
+          )}
+        </div>
+    */
 
     return (
       <div>
-
         <div className="projects">
-        {this.props.projects.map((project, index) =>
-          <ProjectElement
-            key={index}
-            project={project}
-            isSelected={selector.index == index}
-            isOpenMenu={selector.index == index && selector.openMenu}
-            menuIndex={selector.menuIndex}
-            menuType={selector.menuType} /> 
-            )
-        }
-        </div>
-        <div className="hogefuga">
-         <Pagination />
+              <div>
+                <Pagination data={this.props.projects} selector={selector}>
+                  <ShowingResults />
+                </Pagination>
+              </div>
         </div>
       </div>
     );
   }
 
   componentWillReceiveProps(nextProps) {
+    // console.log('----- componentWillReceiveProps in ProjectLis.jsx -----');
+    // console.dir(nextProps);
     if(nextProps.isFetching) {
       return;
     }
     if(this.props.route['path'] !== nextProps.route['path']) {
-      if(nextProps.route['path'] === 'myprojects') {
+      if(nextProps.route['path'] === 'myprojects') {// myProjectの場合，自分が作成したprojectのみを引っ張ってくる
         api.getOwnProjects();
-      } else {
+      } else {// そうじゃない場合，全部引っ張ってくる
         api.getAllProjects();
       }
     }
@@ -81,4 +126,5 @@ function mapStateToProps(state) {
   };
 }
 
+// mapStateToPropsでstateを渡す
 export default connect(mapStateToProps)(ProjectList);
