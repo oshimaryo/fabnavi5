d4b08cb5b120336855ad7c8ec8e322627b13cb6f
fmsuvM <k222.tkd.fms@gmail.com>
2017-03-14 15:57:02 +0900

    add detail reducer

--- app/assets/javascripts/client/reducers/index.js
+++ app/assets/javascripts/client/reducers/index.js
@@ -5,6 +5,7 @@ import player from'./player';
 import user from'./user';
 import manager from'./manager';
 import frame from'./frame';
+import detail from './detail';
 import errors from'./errors';
 
 const debug = Debug('fabnavi:reducer');
@@ -14,5 +15,6 @@ export default combineReducers({
   user,
   manager,
   frame,
+  detail,
   errors
 });
