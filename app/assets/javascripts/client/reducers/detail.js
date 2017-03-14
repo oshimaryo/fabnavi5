f9842a2166d8e782b233d7b113ba9a6ff5663ffc
fmsuvM <k222.tkd.fms@gmail.com>
2017-03-15 02:56:15 +0900

    change initialState

--- app/assets/javascripts/client/reducers/detail.js
+++ app/assets/javascripts/client/reducers/detail.js
@@ -6,11 +6,11 @@ const debug = Debug('fabnavi:reducer:detail');
 
 const initialState = {
     mode: 'projectDetail'
-}
+};
 
 export default function detailReducer(state = initialState, action){
     switch(action.type){
-        case Act.DETAIL_EXIT :
+        case Act.DETAIL_EXIT:
             debug("detail exit, nothing to do");
             return initialState;
         default:
