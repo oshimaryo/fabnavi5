ebca3534b71935d8b999f0669d47e3e54640b699
fmsuvM <k222.tkd.fms@gmail.com>
2017-03-15 02:56:37 +0900

    add detail state to initialState

--- app/assets/javascripts/client/reducers/frame.js
+++ app/assets/javascripts/client/reducers/frame.js
@@ -1,10 +1,20 @@
 import Debug from'debug';
 
-import Act from"../actions/Types";
+import Act from "../actions/Types";
 
 const debug = Debug('fabnavi:reducer:frame');
 
-const initialState = location.pathname.split('/')[1] === 'play' ? 'player' : 'manager';
+// const initialState = location.pathname.split('/')[1] === 'play' ? 'player' : 'manager';
+const initialState;
+const pathName = location.pathname.split('/'[1]);
+if(pathName === 'play'){
+  initialState = 'player';
+} else if(pathName === 'detail'){
+  initialState = 'detail';
+} else {
+  initialState = 'manager;'
+}
+
 
 export default function frameReducer(state = initialState, action){
   switch(action.type){
