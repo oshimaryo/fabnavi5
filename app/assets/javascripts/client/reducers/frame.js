0d05c941b22fbd3e942ca9455866cccac37c69b8
fmsuvM <k222.tkd.fms@gmail.com>
2017-03-15 11:42:10 +0900

    return to original initialState

--- app/assets/javascripts/client/reducers/frame.js
+++ app/assets/javascripts/client/reducers/frame.js
@@ -4,10 +4,7 @@ import Act from "../actions/Types";
 
 const debug = Debug('fabnavi:reducer:frame');
 
-const initialState = 
-    (location.pathname.split('/')[1] === 'play') ? 'player' 
-  : (location.pathname.split('/')[1] === 'detail') ? 'detail'
-  : 'manager' ;
+const initialState = location.pathname.split('/')[1] = 'play' ? 'player' : 'managers';
 
 export default function frameReducer(state = initialState, action){
   switch(action.type){
